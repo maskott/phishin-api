8f8ea1537b9934008ab26284bc47d6d1abe4da94
Mike Skott <mike@skott.us>
2020-03-17 22:56:59 -0400

    hmm whatever i guess

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+var config = require('./config')
 
 const axios = require('axios')
 const { send } = require('micro')
 const microCors = require('micro-cors')
 const cors = microCors({ allowMethods: ['GET'] })
-const DOMAIN = 'http://phish.in/api/v1'
-const varToken = '3781a190cb0140f485c533cf197ecee4e8e20e92385ed71e842f98c87c79d8d52043ef17088e8dbe3b25edf4401f8f93'
+const DOMAIN = config.api_url
+const varToken = config.api_token
 
 const handler = async function(req, res) {
   const params = req.url
